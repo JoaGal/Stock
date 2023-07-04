b8b91bccc323862289a141f5baa6384e7686f50c
JoaGal <joaquingaldeano7@gmail.com>
2023-07-04 12:22:00 -0300

    Changes Server

--- server/index.js
+++ server/index.js
@@ -1,17 +1,24 @@
 const express = require("express");
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 const mysql = require("mysql");
 const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
 
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "clients_crud",
+// });
+
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "clients_crud",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
