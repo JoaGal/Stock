3afaacf6c4773973d6e5d4f4dbc36a14571edbc5
JoaGal <joaquingaldeano7@gmail.com>
2024-01-29 17:16:53 -0300

    logout correction

--- client/src/hooks/useControlUser.jsx
+++ client/src/hooks/useControlUser.jsx
@@ -57,7 +57,7 @@ export const useControlUser = () => {
   //Logout User
   const logoutUser = () => {
     localStorage.removeItem("token");
-    navigate("/");
+    window.location.reload();
   };
 
   //get User
